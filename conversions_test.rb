d17830f886f833712dfa4463a4683340677ec8bc
Nisse Lovendahl <nisselovendahl@gmail.com>
2020-10-28 21:40:21 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,3 +1,8 @@
 require_relative './conversions.rb'
 
 puts "ounces to grams, when given 0, returns 0.0"
+answer = ounces_to_grams(0)
+if answer == 0.0
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
