a0385dd14fff72e16b3fdd763c1ed02ffa8ed836
Nisse Lovendahl <nisselovendahl@gmail.com>
2020-10-28 21:52:48 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -2,8 +2,9 @@ require_relative '../conversions.rb'
 
 describe "conversions" do
   describe "ounces_to_grams" do
-    it "given 0, returns 0.0"
+    it "given 0, returns 0.0" do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
+    end
   end
 end
