ab0dd6a743ad392cc600f001f141c33e72cc23d9
Nisse Lovendahl <nisselovendahl@gmail.com>
2020-10-28 21:41:59 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -8,4 +8,10 @@ else
   puts "Test failed: got #{answer} instead"
 end
 
-puts "ounces_to_grams, when given 1, returns 28."
+puts "ounces to grams, when given 1, returns 28.3495"
+answer = ounces_to_grams(0)
+if answer == 0.0
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
